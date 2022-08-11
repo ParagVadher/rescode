dfc764050236a3cbecc6208fdc277914bc1d0526
ParagVadher <vadherparag@gmail.com>
2022-08-12 01:47:36 +0530

    roter0

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+//use express router - last line from index.js in routes brings it to you
+app.use('/', require('./routes'));
+
 app.listen(port, function(err){
     if(err){
         console.log(`The connection was unsuccessful: ${err}`);
