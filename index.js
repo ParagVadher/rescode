db00d6b2e1579cbbe0a572f7dc9f8197a3738e0f
ParagVadher <vadherparag@gmail.com>
2022-08-14 23:35:28 +0530

    setting up a view engine

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const port = 8000;
 //use express router - last line from index.js in routes brings it to you
 app.use('/', require('./routes'));
 
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
 app.listen(port, function(err){
     if(err){
         console.log(`The connection was unsuccessful: ${err}`);
