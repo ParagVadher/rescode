d3abfe20af2625c4609b8e3555b794907f1558d6
ParagVadher <vadherparag@gmail.com>
2022-10-21 03:10:53 +0530

    styling login and signup

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,13 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/rescode_development');
 
+// mongodb://localhost/rescode_development
+
+// to connect to mongo's prod database
+// mongodb+srv://Paragv0:mLrlQv85Yx6iGa5r@rescode.rzpgtil.mongodb.net/?retryWrites=true&w=majority
+// Paragv0
+// mLrlQv85Yx6iGa5r
+
 const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'Error connecting to db'));
