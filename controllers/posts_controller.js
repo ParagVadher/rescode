a4742d1fa9b77662a782d57efb3a6c4b9cc3405c
ParagVadher <vadherparag@gmail.com>
2022-10-30 01:10:04 +0530

    adding jquery ajax

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -4,11 +4,21 @@ const Comment = require('../models/comment');
 module.exports.create = async function(req, res){
 
     try {
-        await Post.create({
+        let post = await Post.create({
             content: req.body.content,
             user: req.user._id
         });
-    
+        
+        if(req.xhr){
+            return res.status('200').json({
+                data:{
+                    post: post
+                },
+                message: "Post Created successfully!"
+            });
+        }
+
+        req.flash('success', 'Post published!');
         return res.redirect('back');
     } catch (err) {
         console.log('Error aa gaya: ', err);
