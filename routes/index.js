67815a549c875ebc492b80eb9d7ec595c5e27aea
ParagVadher <vadherparag@gmail.com>
2022-11-07 23:41:24 +0530

    setting directory structure for API

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,8 @@ const homeController = require('../controllers/home_controller');
 router.get('/', homeController.home);
 router.get('/gamefeed', homeController.gamefeed);
 
+router.use('/api', require('./api'));
+
 router.use('/users', require('./users'));
 router.use('/about', require('./about'));
 router.use('/posts', require('./posts'));
